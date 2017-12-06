1ddefe657926a9c0be36ea5eecb4be3d2b5dbd3f
arghwoo <arghwoo@gmail.com>
2017-12-05 21:42:25 -0800

    [What] Implement update/query document

--- src/main/scala/MongoDBManager.scala
+++ src/main/scala/MongoDBManager.scala
@@ -15,16 +15,39 @@ class MongoDBManager private {
     usersCollection.insert(dbObj)
   }
 
+  def usersUpdateDoc(request: UserResPutRequest): Unit = {
+    val dbObj = MongoDBObject("user_id" -> request.id, "first_name" -> request.first_name,
+      "last_name" -> request.last_name, "age" -> request.age, "city" -> request.city)
+    usersCollection.findAndModify(MongoDBObject("user_id" -> request.id), dbObj)
+  }
+
+  def userQueryDoc(userId: String): String = {
+    usersCollection.findOne(MongoDBObject("user_id" -> userId)).mkString
+  }
+
   def usersDeleteDoc(userId: String): Unit = {
     val query = MongoDBObject("user_id" -> userId)
     usersCollection.findAndRemove(query)
   }
+
+
 
   def booksInsertDoc(request: BookResPostRequest): Unit = {
     val dbObj = MongoDBObject("isbn" -> request.isbn, "name" -> request.name,
       "category" -> request.category)
     booksCollection.insert(dbObj)
   }
+
+  def bookUpdateDoc(request: BookResPutRequest): Unit = {
+    val dbObj = MongoDBObject("isbn" -> request.isbn, "name" -> request.name,
+      "category" -> request.category)
+    booksCollection.findAndModify(MongoDBObject("isbn" -> request.isbn), dbObj)
+  }
+
+  def bookQueryDoc(isbn: String): String = {
+    booksCollection.findOne(MongoDBObject("isbn" -> isbn)).mkString
+  }
+
 
   def booksDeleteDoc(isbn: String): Unit = {
     println("isbn = " + isbn)
