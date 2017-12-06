afa8676ee3c6d6c0bf94895ef639ae2bedb18e65
arghwoo <arghwoo@gmail.com>
2017-12-05 22:36:07 -0800

    [What] Redefine isbn and user id as _id

--- src/main/scala/MongoDBManager.scala
+++ src/main/scala/MongoDBManager.scala
@@ -10,48 +10,49 @@ class MongoDBManager private {
   val booksCollection = MongoClient()("test")("books_colle")
 
   def usersInsertDoc(request: UserResPostRequest): Unit = {
-    val dbObj = MongoDBObject("user_id" -> request.user_id, "first_name" -> request.first_name,
+    val dbObj = MongoDBObject("_id" -> request.user_id, "first_name" -> request.first_name,
         "last_name" -> request.last_name, "age" -> request.age, "city" -> request.city)
     usersCollection.insert(dbObj)
+    //usersCollection.findAndModify(MongoDBObject("user_id" -> request.user_id), dbObj)
   }
 
   def usersUpdateDoc(request: UserResPutRequest): Unit = {
-    val dbObj = MongoDBObject("user_id" -> request.id, "first_name" -> request.first_name,
+    val dbObj = MongoDBObject("_id" -> request.id, "first_name" -> request.first_name,
       "last_name" -> request.last_name, "age" -> request.age, "city" -> request.city)
-    usersCollection.findAndModify(MongoDBObject("user_id" -> request.id), dbObj)
+    usersCollection.findAndModify(MongoDBObject("_id" -> request.id), dbObj)
   }
 
   def userQueryDoc(userId: String): String = {
-    usersCollection.findOne(MongoDBObject("user_id" -> userId)).mkString
+    usersCollection.findOne(MongoDBObject("_id" -> userId)).mkString
   }
 
   def usersDeleteDoc(userId: String): Unit = {
-    val query = MongoDBObject("user_id" -> userId)
+    val query = MongoDBObject("_id" -> userId)
     usersCollection.findAndRemove(query)
   }
 
 
 
   def booksInsertDoc(request: BookResPostRequest): Unit = {
-    val dbObj = MongoDBObject("isbn" -> request.isbn, "name" -> request.name,
+    val dbObj = MongoDBObject("_id" -> request.isbn, "name" -> request.name,
       "category" -> request.category)
     booksCollection.insert(dbObj)
   }
 
   def bookUpdateDoc(request: BookResPutRequest): Unit = {
-    val dbObj = MongoDBObject("isbn" -> request.isbn, "name" -> request.name,
+    val dbObj = MongoDBObject("_id" -> request.isbn, "name" -> request.name,
       "category" -> request.category)
     booksCollection.findAndModify(MongoDBObject("isbn" -> request.isbn), dbObj)
   }
 
   def bookQueryDoc(isbn: String): String = {
-    booksCollection.findOne(MongoDBObject("isbn" -> isbn)).mkString
+    booksCollection.findOne(MongoDBObject("_id" -> isbn)).mkString
   }
 
 
   def booksDeleteDoc(isbn: String): Unit = {
     println("isbn = " + isbn)
-    val query = MongoDBObject("isbn" -> isbn)
+    val query = MongoDBObject("_id" -> isbn)
     booksCollection.findAndRemove(query)
   }
 
